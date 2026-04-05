faf0534e1a12cabe01be570c0ee8e0327bc0d558
Umesha Nethmi <nethmiumesha313@gmail.com>
2026-04-06 00:17:26 +0530

    Initialize Assessment Service with Domain Entities phase 1

---
+++ src/test/java/com/lms/assessment/AssessmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
