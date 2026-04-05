0e4056ebc58e334944b57138dda8b87c33911f17
Umesha Nethmi <nethmiumesha313@gmail.com>
2026-04-06 00:31:42 +0530

    Add FileStorageException class
    Introduce FileStorageException in com.lms.assessment.exception as a RuntimeException with constructors for message and cause to represent file storage-related errors.

---
+++ src/main/java/com/lms/assessment/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.lms.assessment.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
